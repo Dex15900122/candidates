42679b2f3db5cc195a42d67f0d2982bac0f12cb6
Dex15900122 <e5900122@yahoo.com.tw>
2017-11-25 16:47:57 +0800

    first version

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,56 @@
+class CandidatesController < ApplicationController
+  def index
+    @candidates = Candidate.all
+  end
+
+  def new
+    @candidate=Candidate.new
+  end
+
+  def create
+    @candidate=Candidate.new(candidate_params)
+    if @candidate.save
+      redirect_to candidates_path,notice: "create candidate successful"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @candidate = Candidate.find_by(id: params[:id])
+  end
+
+  def update
+    @candidate=Candidate.find_by(id: params[:id])
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path,notice: "update candidate successful"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy if @candidate
+    redirect_to candidates_path,notice: "delete candidate successful"
+  end
+
+  def vote
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate
+    @candidate.save
+    redirect_to candidates_path,notice: "完成投票！"
+  end
+
+
+
+
+
+
+
+    private
+    def candidate_params
+      params.require(:candidate).permit( :name, :age, :party, :politics)
+    end
+
+end
