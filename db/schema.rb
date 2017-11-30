070f4fec92643c5d858392943ca47faa9b01e3c9
Dex15900122 <e5900122@yahoo.com.tw>
2017-11-30 16:56:42 +0800

    Added portfolio Resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127043733) do
+ActiveRecord::Schema.define(version: 20171130085321) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,22 @@ ActiveRecord::Schema.define(version: 20171127043733) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
